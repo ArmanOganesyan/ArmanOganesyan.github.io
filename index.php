21c5be9bd42b6d966469d9a9c7323e50c720c0c4
ArmanOganesyan <aoganesyan9119@gmail.com>
2018-01-26 14:47:14 +0200

    1

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Shopper
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) :
+
+			get_template_part( 'loop' );
+			
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+do_action( 'shopper_sidebar' );
+get_footer();
